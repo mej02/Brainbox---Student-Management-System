669cb9441ba58f4567f9b7c5c00d1374eeb33a4e
mej02 <fafajem12@gmail.com>
2025-06-10 22:16:27 +0800

    text for debug

--- schoolapi/settings.py
+++ schoolapi/settings.py
@@ -140,3 +140,17 @@ SESSION_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
